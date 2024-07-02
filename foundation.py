9402942145665b797bff4d74afcdc0357940a3c9
technocodermlondie <coding@technofriends.org>
2024-07-02 18:53:09 +0200

    Create foundation.py

---
+++ foundation.py
@@ -0,0 +1,15 @@
+# Mlondiem Technologies
+# Developer Class
+
+# Core Programming Concepts 
+# 
+# 03/07/2024
+# Introduction
+# Variables, data types & operators
+# I/O operations
+
+import os
+
+myname = "Mlondie"
+where = os.getcwd()
+print("Hello! My name is " + myname + '. We are in ' + where)
